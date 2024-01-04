d407b9ba8b76050a1df0368aa340212bb6c8e6c4
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2024-01-04 11:04:49 +0100

    Add Set to manage some products Tests according to definition of done

---
+++ products/product-set.class.js
@@ -0,0 +1,49 @@
+import { Product } from "./product.class"
+
+export class ProductSet {
+    #products = new Set()
+
+    get products() {
+        return this.#products
+    }
+
+    addProduct(product) {
+        if (!(product instanceof Product)) {
+            throw new TypeError('product parameter is not of Product type')
+        }
+        
+        this.#products.add(product)
+        return this
+    }
+
+    /**
+     * Remove product if exists
+     * @param {Product} product 
+     * @returns void
+     */
+    removeProduct(product) {
+        // Is Set have this product ?
+        if (this.#products.has(product)) {
+            this.#products.delete(product)
+        }
+    }
+
+    /**
+     * Update an item according to its ID value
+     *  If ID is mutated it's considered as new product, so, noop
+     * @param {Product} product
+     * @returns void
+     */
+    updateProduct(product) {
+        this.#products.forEach((item) => {
+            if (item.id === product.id) {
+                this.removeProduct(item)
+            }
+        })
+        this.addProduct(product)
+    }
+
+    getSize() {
+        return this.#products.size
+    }
+}
